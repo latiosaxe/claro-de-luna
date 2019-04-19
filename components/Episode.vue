fed3d07e394b9c923c209e61b017a1a95916a6c6
Axel Jesus Gonzalez Alciba <latiosaxe@Axels-MacBook-Pro.local>
2019-04-18 20:08:57 -0500

    Update sesiones

--- components/Episode.vue
+++ components/Episode.vue
@@ -1,26 +1,10 @@
 <template>
   <div>
     <div class="episode">
-      <div class="container">
-        <div class="columns is-2">
-          <div class="column episode__video">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/uBco8a6odH4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div>
-          <div class="column episode__content">
-            <div class="episode__content__title">Ultraman</div>
-            <div class="episode__content__date">26 de Marzo</div>
-            <div class="episode__content__date">Sesión y entrevista en vivo</div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="episode">
-
       <div class="container inverted">
         <div class="columns ">
           <div class="column episode__content">
-            <div class="episode__content__title">Maskatesta</div>
+            <div class="episode__content__title">Maskatesta Sesión</div>
             <div class="episode__content__date">2 de Abril</div>
             <div class="episode__content__date">Sesión y entrevista en vivo</div>
           </div>
@@ -31,13 +15,43 @@
         </div>
       </div>
     </div>
+    <div class="episode">
+      <div class="container inverted">
+        <div class="columns ">
+          <div class="column episode__content">
+            <div class="episode__content__title">Maskatesta Entrevista</div>
+            <div class="episode__content__date">2 de Abril</div>
+            <div class="episode__content__date">Sesión y entrevista en vivo</div>
+          </div>
+          <div class="column episode__video">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/Y4rOzs2ZETc?start=1540" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="episode">
+      <div class="container">
+        <div class="columns">
+          <div class="column episode__video">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/uBco8a6odH4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+          <div class="column episode__content">
+            <div class="episode__content__title">Ultraman Sesión</div>
+            <div class="episode__content__date">26 de Marzo</div>
+            <div class="episode__content__date">Sesión y entrevista en vivo</div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style>
   .episode{ padding: 100px 0; display: block; }
   .episode__content{ color: #FFF; margin-top: auto; }
-  .episode__content__title{ font-size: 100px; transform: translateX(-10%) }
+  .episode__content__title{ font-size: 100px; line-height: 100%; transform: translateX(-10%) }
   /* .episode__content__date{ text-align: right } */
   iframe{ height: 129%; }
 
